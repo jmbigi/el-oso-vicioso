a77aaae3184392afea2b9a00143a53ba69ee68d1
appnew-jb <126823366+appnew-jb@users.noreply.github.com>
2025-06-04 11:45:14 -0400

    Add configuration file and installation script for Fooocus

---
+++ install_focus.sh
@@ -0,0 +1,10 @@
+set -e
+# ---------------------------------------------------
+# 🎨 PASO 3: INSTALAR FOOOCUS (imágenes SD)
+# ---------------------------------------------------
+git clone https://github.com/lllyasviel/Fooocus.git
+cd Fooocus
+python3 -m venv venv
+source venv/bin/activate
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
+pip install -r requirements.txt
